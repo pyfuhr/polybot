d48b8630000494818f66094d3cf3ddd3f9d8c42a
pyfuhr <pyfuhr@gmail.com>
2021-09-14 18:41:45 +0300

    init

---
+++ spbpu_ruz.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+
+class Lesson:
+    def __init__(self, name, time, type, groups, teacher, place):
+        self.name = name
+        self.time = time
+        self.type = type
+        self.groups = groups
+        self.teacher = teacher
+        self.place = place
+    
+    def __repr__(self):
+        return self.name + ' at ' + self.time
+
+def getRuz(date=''):
+    if date:
+        date = '?date=' + date
+    s = requests.Session()
+    with open('111.htm', 'w') as data:
+        x = s.get('https://ruz.spbstu.ru/faculty/94/groups/34295' + date)
+        data.write(x.content.decode('cp1251', errors='ignore'))
+
+    with open('111.htm', 'r', encoding='cp1251') as data:
+        x = ''.join(data.readlines()).encode('cp1251').decode('utf8', errors='ignore')
+    ruz = []
+    soup = BeautifulSoup(x, 'html.parser')
+    week = soup.find_all('li', attrs={'class': 'schedule__day'})
+    for day in week:
+        daysoup = BeautifulSoup(str(day), 'html.parser')
+        rday = []
+        #daysoup.find('div').contents[0] # date
+        dayofweek = 0
+        for lesson in daysoup.find_all('li', attrs={'class': 'lesson'}):
+            less = Lesson(*[None]*6)
+            lessonsoup = BeautifulSoup(str(lesson), 'html.parser')
+            for attr in lessonsoup.find_all('li'):
+                attrsoup = BeautifulSoup(str(attr), 'html.parser')
+                for div in attrsoup.find_all('div', attrs={'class': 'lesson__subject'}):
+                    divsoup =  BeautifulSoup(str(div), 'html.parser')
+                    tmp = divsoup.find_all('span')
+                    less.name = tmp[5].contents[0]
+                    if less.name[0].islower():
+                        less.name = 'И' + less.name
+                    less.time = tmp[1].contents[0] + tmp[2].contents[0] + tmp[3].contents[0]
+                    
+                less.type = attrsoup.find('div', attrs={'class': 'lesson__type'}).contents[0]
+                less.groups = [i[2:-4] for i in re.findall('\">[0-9\/]{13}<\/a>', str(attrsoup.find_all('span', attrs={'class': 'lesson__group'})), re.M|re.I)]
+                less.teacher = re.search('\">[А-Яа-я\-A-Za-z\s]+<\/span><\/a><\/div>', str(attrsoup.find('div', attrs={'class': 'lesson__teachers'}).contents[0])).group()[2:-17]
+                spansoup = BeautifulSoup(str(attrsoup.find('div', attrs={'class': 'lesson__places'}).contents[0]), 'html.parser')
+                span = spansoup.find_all('span')
+                less.place = span[1].contents[0] + ', ' + span[6].contents[0] + span[7].contents[0]
+            rday.append(less)
+        ruz.append(rday.copy())
+    ruz.append([])
+    ruz.append([])
+    return ruz
+
+def getExtRuz(date=''):
+    if not date:
+        date = (datetime.datetime.now() - datetime.timedelta(days=datetime.datetime.now().weekday())).strftime('%Y-%m-%d')
+    r = getRuz(date)
+    d = datetime.datetime.strptime(date, '%Y-%m-%d')
+    d -= datetime.timedelta(days=d.weekday())
+    d += datetime.timedelta(days=7)
+    [r.append(i) for i in getRuz(d.strftime('%Y-%m-%d'))]
+    return r
---
+++ tbl.py
@@ -0,0 +1,93 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import random
+from spbpu_ruz import getExtRuz
+import datetime
+import sqlite3
+
+def workWithBD(command: str):
+    try:
+        conn = sqlite3.connect("C:/Users/user/Desktop/z.db")
+        cursor = conn.cursor()
+        cursor.execute(command)
+        conn.commit()
+        return cursor.fetchall()
+    except Exception as e:
+        return e
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 65536)})
+
+token = '4fd114fff83dae203a5680c865dd86639291c231b74444c20ddd451216e985d8268c79b142c05b811fd92'
+vk = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(vk)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text.lower()
+            if request == "пары" or request == "-n":
+                ruz = getExtRuz()
+                if ruz[datetime.datetime.now().weekday()]:
+                    tmp = []
+                    for i in ruz[datetime.datetime.now().weekday()]:
+                        if datetime.time.fromisoformat(i.time.split('-')[1].zfill(5) + ':00') < datetime.datetime.now().time():
+                            tmp.append('🟥' + str(i))
+                        elif datetime.time.fromisoformat(i.time.split('-')[1].zfill(5) + ':00') > datetime.datetime.now().time() and\
+                            datetime.time.fromisoformat(i.time.split('-')[0].zfill(5) + ':00') < datetime.datetime.now().time():
+                            tmp.append('🟨' + str(i))
+                        else:
+                            tmp.append('0' + str(i))
+                    write_msg(event.user_id, str('\n'.join(tmp)))
+                else:
+                    write_msg(event.user_id, 'Сегодня пар нет')
+            elif request == "завтра" or request == "-t":
+                ruz = getExtRuz()
+                if ruz[datetime.datetime.now().weekday() + 1]:
+                    write_msg(event.user_id, str('\n'.join(['🟦' + str(i) for i in ruz[datetime.datetime.now().weekday() + 1]])))
+                else:
+                    write_msg(event.user_id, 'Сегодня пар нет')
+            elif request == "-h":
+                write_msg(event.user_id, """-n или \"пары\" для получения списка сегодняшних пар
+-t или \"завтра\" для получения списка завтрашних пар
+-z для получения списка задач
+-d для удаление задач
+-a для создания задач
+-h что-бы ещё раз увидеть это окно""")
+            elif request == "-z":
+                try:
+                    tmp = []
+                    for i in workWithBD('SELECT * FROM main'):
+                        if i[1] > datetime.datetime.now().timestamp():
+                            tmp.append(f'⦁{i[0]}. ' + i[2] + f'\n ‍ ‍ ‍ ‍ ‍•‍{str(datetime.datetime.fromtimestamp(i[1]))} [id{i[3]}|A]')
+                        else:
+                            tmp.append('🗑 ' + i[2])
+                    if tmp:
+                        write_msg(event.user_id, '\n'.join(tmp))
+                        workWithBD(f'DELETE FROM main WHERE datetime < {int(datetime.datetime.now().timestamp())}')
+                    else:
+                        write_msg(event.user_id, 'Пусто')
+                except Exception as e:
+                    write_msg(event.user_id, '❗' + str(e))
+            elif request[:2] == '-a':
+                d = request.split()
+                try:
+                    tmp = int(datetime.datetime.strptime(d[1], "%Y-%m-%d").timestamp() + 64800)
+                except Exception as e:
+                    write_msg(event.user_id, "Неверно указана дата (гггг-мм-дд)\nБудет использована завтрашняя дата\n❗ " + str(e))
+                    tmp = int((datetime.datetime.now() + datetime.timedelta(days=1)).timestamp() + 64800)
+                tmp = workWithBD(f'INSERT INTO main (datetime, desc, author) VALUES({tmp},' +\
+                    f' \"{" ".join(d[2:])}\", \"{event.user_id}\")')
+                if type(tmp) == list:
+                    write_msg(event.user_id, "✔Успех")
+                elif type(tmp) == Exception:
+                    write_msg(event.user_id, '❗' + str(tmp))
+            elif request[:2] == '-d':
+                d = request.split()[1]
+                tmp = workWithBD(f'DELETE FROM main WHERE id={d}')
+                if type(tmp) == list:
+                    write_msg(event.user_id, "✔Успех")
+                elif type(tmp) == Exception:
+                    write_msg(event.user_id, '❗' + str(tmp))
+            else:
+                write_msg(event.user_id, "-h для помощи")
